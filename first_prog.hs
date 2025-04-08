03a84674b0e9d7d5c71658858d6a73095600048b
likanug <likanug.g@qq.com>
2025-04-08 21:33:41 +0800

    重构邮件生成函数，增加可读性和模块化

--- first_prog.hs
+++ first_prog.hs
@@ -6,6 +6,13 @@ messyMain = do
     title <- getLine
     print "Who is the author?"
     author <- getLine
+
     print ("Dear " ++ recipient ++ ",\n" ++
         "Thanks for buying " ++ title ++ ".\nthanks,\n" ++
         author)
+
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+fromPart author = "thanks,\n" ++ author
+titlePart title = "Thanks for buying " ++ title ++ ".\n"
+createEmail recipient title author =
+    toPart recipient ++ titlePart title ++ fromPart author
