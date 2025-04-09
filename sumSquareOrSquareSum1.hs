0d5a839fc697ddb194fe22d7f112698bb17a9c51
likanug <likanug.g@qq.com>
2025-04-09 21:42:23 +0800

    简化 sumSquareOrSquareSum 函数逻辑，直接计算平方和与和的平方的较大值

--- sumSquareOrSquareSum1.hs
+++ sumSquareOrSquareSum1.hs
@@ -1,5 +1,3 @@
-sumSquareOrSquareSum x y = if sumSquare > squareSum
-                            then sumSquare
-                            else squareSum
-  where sumSquare = x^2 + y^2
-        squareSum = (x + y)^2
+sumSquareOrSquareSum x y = if x^2 + y^2 > (x + y)^2
+                            then x^2 + y^2
+                            else (x + y)^2
