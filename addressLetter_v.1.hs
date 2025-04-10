1077e93718fa9c590c3300d59ee57a3cdb93b308
likanug <likanug.g@qq.com>
2025-04-10 22:44:26 +0800

    重构地址信函数，添加旧版办公室地址处理逻辑

--- addressLetter_v.1.hs
+++ addressLetter_v.1.hs
@@ -3,3 +3,23 @@ addressLetter name location = nameText ++ " - " ++ location
     nameText = (fst name) ++ " " ++ (snd name)
 
 -- nameText = "Dear " ++ name ++ ",\n"
+
+-- San Francisco office
+sfOffice name =
+  if lastName < "L"
+    then nameText ++ " -PO Box 1234 - San Francisco, CA, 94111"
+    --- last name 在L后面的
+    else nameText ++ " -PO Box 1010 - San Francisco, CA, 94109"
+  where
+    lastName = snd name
+    nameText = (fst name) ++ " " ++ (snd name)
+
+-- New York office
+nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
+  where
+    nameText = (fst name) ++ " " ++ (snd name)
+
+-- Reno office
+renoOffic name = nameText ++ " - PO Box 456 - Reno, NV 89523"
+  where
+    nameText = snd name
