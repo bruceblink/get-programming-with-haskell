918d29af8cbd98a4db88cfec398cf70c3273901a
likanug <likanug.g@qq.com>
2025-04-10 08:44:48 +0800

    新增 VSCode 配置文件，包含 Haskell 调试器和构建任务设置

--- sumSquareOrSquareSum1.hs
+++ sumSquareOrSquareSum1.hs
@@ -1,4 +1,18 @@
-sumSquareOrSquareSum x y = (\sumSquare squareSum -> 
-                            if sumSquare > squareSum
+sumSquareOrSquareSum x y = let sumSquare = x^2 + y^2  -- let表达式
+                               squareSum = (x + y)^2
+                           in --函数体开始
+                            -- if要一个空格
+                            if sumSquare > squareSum  --let表达式
                             then sumSquare
-                            else squareSum) (x^2 + y^2) ((x + y)^2)
+                            else squareSum
+
+overwrite x = let x = 2
+              in
+                let x = 3
+                in
+                  let x = 4
+                  in
+                    let x = 5
+                    in
+                      x
+-- 变量覆盖
