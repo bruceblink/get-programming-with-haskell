434e55474b5b099070a3da3051eee1dba75936b7
likanug <likanug.g@qq.com>
2025-04-16 13:50:16 +0800

    格式化代码

--- part1-capstone/robot.hs
+++ part1-capstone/robot.hs
@@ -1,32 +1,42 @@
--- robot的构造函数,有3个属性
+module Lesson10 where
+
 robot (name, attack, hp) = \message -> message (name, attack, hp)
 
--- 添加获取对象属性的辅助函数
+killerRobot = robot ("Kill3r", 25, 200)
 name (n, _, _) = n
 attack (_, a, _) = a
 hp (_, _, hp) = hp
 
--- 添加get访问器函数
 getName aRobot = aRobot name
 getAttack aRobot = aRobot attack
 getHP aRobot = aRobot hp
 
--- 添加set访问器函数
-setName aRobot newName = aRobot (\(n,a,h) -> robot (newName, a, h))
-setAttack aRobot newAttack = aRobot (\(n,a,h) -> robot (n, newAttack, h))
-setHP aRobot newHP = aRobot (\(n,a,h) -> robot (n, a, newHP))
+setName aRobot newName = aRobot (\(n, a, h) -> robot (newName, a, h))
+setAttack aRobot newAttack = aRobot (\(n, a, h) -> robot (n, newAttack, h))
+setHP aRobot newHP = aRobot (\(n, a, h) -> robot (n, a, newHP))
 
--- 添加打印状态的函数(类比 Java中的toString)
-printRobot aRobot = aRobot (\(n, a, h) -> n ++ 
-                                        " attack: " ++ (show a) ++ 
-                                        " hp:" ++ (show h))
+printRobot aRobot =
+  aRobot (\(n, a, h) -> n ++ " attack:" ++ show a ++ " hp:" ++ show h)
 
--- 添加damage函数
-damage aRobot attackDamage = aRobot (\(n, a, h) -> 
-                                       robot (n, a, h-attackDamage))
+damage aRobot attackDamage =
+  aRobot (\(n, a, h) -> robot (n, a, h - attackDamage))
 
--- 定义fight函数
+--
+
 fight aRobot defender = damage defender attack
-    where attack = if getHP aRobot > 10
-                   then getAttack aRobot
-                   else 0
+  where attack = if getHP aRobot > 10 then getAttack aRobot else 0
+
+--
+-- Why stateless programming matters
+--
+
+fastRobot = robot ("speedy", 15, 40)
+slowRobot = robot ("slowpoke", 20, 30)
+
+slowRobotRound1 = fight fastRobot slowRobot 
+fastRobotRound1 = fight slowRobotRound1 fastRobot 
+slowRobotRound2 = fight fastRobotRound1 slowRobotRound1 
+fastRobotRound2 = fight slowRobotRound1 fastRobotRound1 
+slowRobotRound3 = fight fastRobotRound2 slowRobotRound2 
+fastRobotRound3 = fight slowRobotRound3 fastRobotRound2
+
