0b064e8661e0afd3caa7af28a52140e1940e3542
likanug <likanug.g@qq.com>
2025-04-16 09:13:46 +0800

    添加改变cup状态的drink函数

--- part1-capstone/cup.hs
+++ part1-capstone/cup.hs
@@ -1,3 +1,10 @@
 --cup f10z = \_ -> f10z
 
 cup flOz = \message -> message flOz -- 添加命名参数的cup"构造函数"
+
+getOz aCup = aCup (\f10z -> f10z)  --添加对象访问器(成员函数)
+
+-- drink message 更新cup的状态
+drink aCup ozDrank = cup (f10z - ozDrank)  -- drink message 更新cup的状态
+  where f10z = getOz aCup  -- 获取对象原先的状态
+
