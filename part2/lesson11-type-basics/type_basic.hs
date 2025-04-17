a58c1959ffb8e1e0ffe0061b641ed7d8bbec5d97
likanug <likanug.g@qq.com>
2025-04-17 09:54:29 +0800

    定义List类型

--- part2/lesson11-type-basics/type_basic.hs
+++ part2/lesson11-type-basics/type_basic.hs
@@ -15,3 +15,14 @@ interesRate = 0.375
 
 isFun :: Bool
 isFun = True
+
+-- List类型
+values :: [Int]
+values = [1, 2, 3]
+
+testScores :: [Double]
+testScores = [0.99, 0.7, 0.8]
+
+letters :: [Char]
+letters = ['a', 'b', 'c']
+
