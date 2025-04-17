5f7bb83f809e95df2684be6eb6cbd47d0788b505
likanug <likanug.g@qq.com>
2025-04-17 09:50:21 +0800

    定义通用类型 Char Double 和 Bool

--- part2/lesson11-type-basics/type_basic.hs
+++ part2/lesson11-type-basics/type_basic.hs
@@ -1,6 +1,17 @@
 module Lesson11 where
+import Distribution.Compat.CharParsing (letter)
 x :: Int
 x = 2
 
 y :: Integer
 y = 2
+
+--- 通用类型 Char Double 和 Bool
+letter :: Char
+letter = 'a'
+
+interesRate :: Double
+interesRate = 0.375
+
+isFun :: Bool
+isFun = True
