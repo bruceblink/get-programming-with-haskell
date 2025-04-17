694ef1d49bf19896634cb684cd37e440a0d954e5
likanug <likanug.g@qq.com>
2025-04-17 10:14:42 +0800

    定义元组类型

--- part2/lesson11-type-basics/type_basic.hs
+++ part2/lesson11-type-basics/type_basic.hs
@@ -33,3 +33,16 @@ aPet = "cat"
 
 anotherPet :: String
 anotherPet = "dog"
+
+-- 元组
+-- 相同类型
+ageAndHeight :: (Int, Int)
+ageAndHeight = (34, 34)
+
+-- 不同类型
+firstLastMiddle :: (String, String, Char)
+firstLastMiddle = ("Oscar", "Grouch", 'D')
+
+streetAddress :: (Int, String)
+streetAddress = (123, "Happy St.")
+
