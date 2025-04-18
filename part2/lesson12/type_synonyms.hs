58161445d360bce9f39683e0b0cb73d07c35ffa9
likanug <likanug.g@qq.com>
2025-04-18 11:16:25 +0800

    给数据定义类型

---
+++ part2/lesson12/type_synonyms.hs
@@ -0,0 +1,71 @@
+module Lesson12 where
+import Data.Text.Internal.Fusion (Stream)
+patientInfo :: FirstName -> LastName -> Age -> Height -> String
+patientInfo fname lname age height = name ++ " " ++ ageHeight
+  where name = lname ++ ", " ++ fname
+        ageHeight = "(" ++ show age ++ "yrs. " ++ show height ++ "in.)"
+
+-- type synonyms: FirstName, LastName, Age, and Height
+type FirstName = String
+type LastName = String
+type Age = Int
+type Height = Int
+-- type synonyms: PatientName
+type PatientName = (String, String)
+-- PatientName's  help function
+firstName :: PatientName -> String 
+firstName patient = fst patient 
+lastName :: PatientName -> String 
+lastName patient = snd patient
+
+-- 创造新类型
+data Sex = Male | Female
+-- sexInitial 函数
+sexInitial :: Sex -> Char
+sexInitial Male = 'M'
+sexInitial Female = 'F'
+
+-- Defining the type RhType
+data RhType = Pos | Neg
+-- Defining the type ABOType
+data ABOType = A | B | AB | O
+-- Combining ABOType and RhType to create BloodTyp
+data BloodType = BloodType ABOType RhType
+-- BloodType
+patient1BT :: BloodType 
+patient1BT = BloodType A Pos 
+patient2BT :: BloodType 
+patient2BT = BloodType O Neg 
+patient3BT :: BloodType
+patient3BT = BloodType AB Pos
+
+-- Displaying your types: showRh, showABO, showBloodType
+showRh :: RhType -> String
+showRh Pos = "+"
+showRh Neg = "-"
+showABO :: ABOType -> String
+showABO A = "A"
+showABO B = "B"
+showABO AB = "AB"
+showABO O = "O"
+showBloodType :: BloodType -> String
+showBloodType (BloodType abo rh) = showABO abo ++ showRh rh
+
+-- Defining the canDonateTo function
+canDonateTo :: BloodType -> BloodType -> Bool 
+canDonateTo (BloodType O _) _ = True 
+canDonateTo _ (BloodType AB _) = True 
+canDonateTo (BloodType A _) (BloodType A _) = True 
+canDonateTo (BloodType B _) (BloodType B _) = True 
+canDonateTo _ _ = False --otherwise
+
+-- Support different names: MiddleName and Name
+type MiddleName = String
+data Name = Name FirstName LastName 
+          | NameWithMiddle FirstName MiddleName LastName
+
+-- Displaying multiple constructors: showName
+showName :: Name -> String
+showName (Name f l) = f ++ " " ++ l
+showName ((NameWithMiddle f m l)) = f ++ " " ++ m ++ " " ++ l
+
