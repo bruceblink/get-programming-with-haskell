9498819932fa208d868a9b7e8e80805fe12c32e0
likanug <likanug.g@qq.com>
2025-04-24 10:44:44 +0800

    增加Triple类型及其相关函数

--- part3/lesson18/Lesson18.hs
+++ part3/lesson18/Lesson18.hs
@@ -1,1 +1,42 @@
+module Lesson18 where
+import Data.Char (toLower)
+
 data Box a = Box a deriving Show
+-- 定义Triple
+data Triple a = Triple a a a deriving Show
+
+-- 定义一个空间中的3D point
+type Point3D = Triple Double
+aPoint :: Point3D
+aPoint = Triple 0.1 53.2 12.3
+
+-- 定义人名
+type FullName = Triple String
+aPerson :: FullName
+aPerson = Triple "Howard" "Phillips" "Lovecraft"
+
+type Initials = Triple Char 
+initials :: Initials
+initials = Triple 'H' 'P' 'L'
+
+-- 访问 Triple 类型
+first :: Triple a -> a 
+first (Triple x _ _) = x 
+
+second :: Triple a -> a 
+second (Triple _ x _ ) = x 
+
+third :: Triple a -> a 
+third (Triple _ _ x) = x
+
+-- 将Triple 转换成 list
+toList :: Triple a -> [a]
+toList (Triple x y z) = [x, y, z]
+
+transform :: (a -> a) -> Triple a -> Triple a
+transform  f (Triple x y z) = Triple (f x) (f y) (f z)
+
+{- -- Apply the transformation to initials
+transformedInitials :: Initials
+transformedInitials = transform toLower initials -}
+
