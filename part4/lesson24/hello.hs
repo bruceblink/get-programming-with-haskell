92707a8a760ec4177d74f5468a2aac92d163ffb5
likanug <likanug.g@qq.com>
2025-04-29 11:04:07 +0800

    添加hello.txt和goodbye.txt文件，并实现从hello.txt读取内容并写入goodbye.txt的功能

--- part4/lesson24/hello.hs
+++ part4/lesson24/hello.hs
@@ -1,7 +1,15 @@
 import System.IO
 main :: IO ()
 main = do
-  myFile <- openFile
-            "hello.txt" ReadMode
-  hClose myFile
+  helloFile <- openFile "hello.txt" ReadMode
+  firstLine <- hGetLine helloFile
+  putStrLn firstLine
+  secondLine <- hGetLine helloFile
+  goodbyeFile <- openFile "goodbye.txt" WriteMode
+  hPutStrLn goodbyeFile secondLine
+  hClose helloFile
+  hClose goodbyeFile
   putStrLn "done!"
+
+  -- 从hello.txt 读取第一行写入控制台，
+  -- 然后从hello.txt读取第二行写入goodbye.txt
