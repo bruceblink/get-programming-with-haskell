7252c042125be69aafee46d16aa157f52d3560a8
likanug <likanug.g@qq.com>
2025-04-29 11:41:45 +0800

    添加fileCounts.hs文件，实现字符、单词和行数统计功能

---
+++ part4/lesson24/fileCounts.hs
@@ -0,0 +1,13 @@
+import System.IO
+
+getCounts :: String -> (Int, Int, Int)
+getCounts input = (charCount, wordCount, lineCount)
+  where
+    charCount = length input
+    wordCount = (length . words) input
+    lineCount = (length . lines) input
+
+countsText :: (Int, Int, Int) -> String
+countsText (cc, wc, lc) = unwords ["chars: ", show cc , " words: ", show wc , " lines: ", show lc]
+
+
