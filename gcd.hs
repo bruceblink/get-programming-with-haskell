73a19f5fcd4d0e37cd026fd254ee1efacb975c2a
likanug <likanug.g@qq.com>
2025-05-01 10:15:51 +0800

    更新.gitignore以忽略dist目录，添加CHANGELOG.md和LICENSE文件，删除不再使用的Haskell文件

--- gcd.hs
+++
@@ -1,9 +0,0 @@
-myGCD a b = if remainder == 0
-    then b
-    else myGCD b remainder
- where remainder = a `mod` b
-
-myGCD1 a b = case remainder of
-    0 -> b
-    _ -> myGCD1 b remainder
- where remainder = a `mod` b
