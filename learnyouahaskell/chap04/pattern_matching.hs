4afe6528ee4cb6ad54cf04186797b44c8497ba31
likanug <likanug.g@qq.com>
2025-05-04 21:17:31 +0800

    删除VSCode配置文件，并在Chap04模块中添加自定义length'和sum'函数

--- learnyouahaskell/chap04/pattern_matching.hs
+++ learnyouahaskell/chap04/pattern_matching.hs
@@ -1,3 +1,14 @@
+module Chap04 where
 lucky :: (Integral a) => a -> String  
 lucky 7 = "LUCKY NUMBER SEVEN!"  
 lucky x = "Sorry, you're out of luck, pal!"
+
+-- 自定义 length'
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+-- 自定义 sum
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
