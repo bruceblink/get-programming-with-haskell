91597a23b0130a26b2ef316866058bb9f491c375
likanug <likanug.g@qq.com>
2025-05-04 11:03:51 +0800

    添加countdown和factors函数，扩展Chapter2模块功能

--- Effective-Haskell-Solving-Real-World-Problems/chapter2/Chapter2.hs
+++ Effective-Haskell-Solving-Real-World-Problems/chapter2/Chapter2.hs
@@ -4,3 +4,15 @@ listisEmpty list =
     if null list
     then putStrLn "this list is empty"
     else putStrLn ("the first element of this list is: " <> show (head list))
+
+countdown n = 
+    if n <= 0
+    then []
+    else n : countdown (n - 1)
+
+factors num =
+  factors' num 2
+  where factors' num fact
+          | num == 1 = []
+          | (num `rem` fact) == 0 = fact : factors' (num `div` fact) fact
+          | otherwise   = factors' num (fact + 1)
