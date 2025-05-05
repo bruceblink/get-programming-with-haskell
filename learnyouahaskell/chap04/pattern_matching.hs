d66084256c5529c09fd05e2196c1b313e0194abf
likanug <likanug.g@qq.com>
2025-05-05 10:20:06 +0800

    添加calcBmis

--- learnyouahaskell/chap04/pattern_matching.hs
+++ learnyouahaskell/chap04/pattern_matching.hs
@@ -27,3 +27,8 @@ initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
     where (f:_) = firstname
           (l:_) = lastname
+
+-- 计算BMI使用where
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
