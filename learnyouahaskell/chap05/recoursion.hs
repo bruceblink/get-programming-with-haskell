9e6dea0968e7bb7c39b7e1b72c2b6eef3d42a52f
likanug <likanug.g@qq.com>
2025-05-06 10:37:29 +0800

    添加自定义的take'和reverse’

--- learnyouahaskell/chap05/recoursion.hs
+++ learnyouahaskell/chap05/recoursion.hs
@@ -12,3 +12,18 @@ maximum' (x:xs)
 -- 用法示例： maximum' [10, 32, 1, 15, 62, 101]  -- 返回 101
 
 
+-- 自定义take
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _ 
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+-- 用法示例： take' 2 [1,2,3,4,5]  -- 返回 [1, 2]
+
+-- 自定义reverse
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+-- 用法示例 reverse' [1,2,3,4,5] -- 返回 [5,4,3,2,1] 
