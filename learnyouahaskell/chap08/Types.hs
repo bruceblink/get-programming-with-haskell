d82d9601e1b1db2488595d248549fcbaa3637b4e
likanug <likanug.g@qq.com>
2025-05-08 12:04:23 +0800

    定义二叉搜索树以及插入和查找元素的函数

--- learnyouahaskell/chap08/Types.hs
+++ learnyouahaskell/chap08/Types.hs
@@ -9,3 +9,34 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
 -- 移动矩形
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+-- Record 语法
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+-- 定义一个二叉树
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+-- 定义一个单节点的树
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+-- 树的元素插入
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
+
+-- 树的元素查找
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
