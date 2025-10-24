898b5b7b274dac47542232275d0af4b58b7d2bf1
likanug <likanug.g@qq.com>
2025-10-24 19:10:02 +0800

    新增 Variable.hs 和 Function.hs 文件，定义基本的 Haskell 示例代码，包括字符串和数字拼接、加法和乘法函数。

---
+++ ch01/02.Function.hs
@@ -0,0 +1,10 @@
+module Function where
+
+add :: Int -> Int -> Int
+add x y = x + y
+multiply :: Int -> Int -> Int
+multiply x y = x * y
+-- --- IGNORE ---       
+-- 以上代码定义了两个函数 add 和 multiply，分别用于加法和乘法运算。
+-- --- IGNORE ---
+main = print (add 3 5, multiply 4 6)
