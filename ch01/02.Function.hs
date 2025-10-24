41356dadeee54edb73d45ee0200f5d410c0dc070
likanug <likanug.g@qq.com>
2025-10-24 19:13:50 +0800

    新增 makeGreeting 函数，支持字符串拼接以生成问候语

--- ch01/02.Function.hs
+++ ch01/02.Function.hs
@@ -4,7 +4,9 @@ add :: Int -> Int -> Int
 add x y = x + y
 multiply :: Int -> Int -> Int
 multiply x y = x * y
+
+makeGreeting salutation person = salutation <> ", " <> person <> "!"
 -- --- IGNORE ---       
 -- 以上代码定义了两个函数 add 和 multiply，分别用于加法和乘法运算。
 -- --- IGNORE ---
-main = print (add 3 5, multiply 4 6)
+main = print (add 3 5, multiply 4 6)  -- (8, 24)
