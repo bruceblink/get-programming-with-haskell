834682075a41a0d9a650a669ad4f0866776a83ab
likanug <likanug.g@qq.com>
2025-10-24 19:55:50 +0800

    新增匿名函数和三元组函数示例，扩展 Function.hs 文件

--- ch01/02.Function.hs
+++ ch01/02.Function.hs
@@ -9,7 +9,23 @@ makeGreeting salutation person = salutation <> ", " <> person <> "!"
 
 -- 调用 makeGreeting
 greeting = makeGreeting "Hello" "George" -- "Hello, George!"
--- --- IGNORE ---       
--- 以上代码定义了两个函数 add 和 multiply，分别用于加法和乘法运算。
--- --- IGNORE ---
+
+-- 定义匿名函数 lambdaAdd
+lambdaAdd = \x y -> x + y   
+-- 调用 lambdaAdd
+lambdaResult = lambdaAdd 2 3  -- 5  
+
+-- 调用匿名函数
+greetingLambda = \salutation person -> salutation <> ", " <> person <> "!"
+greetingResult = greetingLambda "Hi" "Alice"  -- "Hi, Alice!"
+
+-- 定义一个函数，返回一个三元组 以下三种写法等价
+makeThruple a b c = (a,b,c)
+lambdaThruple a b = \c -> (a,b,c)
+lambdaThruple' = \a -> \b -> \c -> (a,b,c)
+-- 调用 makeThruple 和 lambdaThruple
+thruple1 = makeThruple 1 2 3          -- (1,2,3)
+thruple2 = lambdaThruple 1 2 3        -- (1,2,3)
+thruple3 = lambdaThruple' 1 2 3      -- (1,2,3)
+
 main = print (add 3 5, multiply 4 6)  -- (8, 24)
