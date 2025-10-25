0b73172a7fa82f8498c7c8e586daa89017f21676
likanug <likanug.g@qq.com>
2025-10-25 11:16:42 +0800

    新增部分应用函数示例，扩展 Function.hs 文件

--- ch01/02.Function.hs
+++ ch01/02.Function.hs
@@ -28,4 +28,18 @@ thruple1 = makeThruple 1 2 3          -- (1,2,3)
 thruple2 = lambdaThruple 1 2 3        -- (1,2,3)
 thruple3 = lambdaThruple' 1 2 3      -- (1,2,3)
 
+-- 使用 partially applied functions(部分应用函数)
+addFive = add 5          -- 部分应用函数，固定第一个参数为5
+resultAddFive = addFive 10  -- 15
+
+-- lambda 表达式的部分应用
+multiplyByTwo = \x -> multiply 2 x  -- 部分应用函数，固定第一个参数为2
+resultMultiplyByTwo = multiplyByTwo 8  -- 16
+-- 另一个部分应用示例
+greetPerson = \person -> makeGreeting "Hello" person  -- 部分应用函数，固定第一个参数为"Hello"
+greetingForBob = greetPerson "Bob"  -- "Hello, Bob!"
+-- 固定参数
+enthusiasticGreet salutation person = makeGreeting (salutation <> "!") person
+enthusiasticGreetingForEve = enthusiasticGreet "Hi" "Eve"  -- "Hi!, Eve!"
+
 main = print (add 3 5, multiply 4 6)  -- (8, 24)
