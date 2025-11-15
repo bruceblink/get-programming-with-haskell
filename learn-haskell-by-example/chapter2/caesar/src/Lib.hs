f0fcea4002b54bf92a9e5cce76f22df0c3b052a7
likanug <likanug.g@qq.com>
2025-11-15 23:08:30 +0800

    feat: 定义字母表类型

--- learn-haskell-by-example/chapter2/caesar/src/Lib.hs
+++ learn-haskell-by-example/chapter2/caesar/src/Lib.hs
@@ -1,6 +1,5 @@
 module Lib          --1 定义模块名
-    ( square        --2 指定导出列表
+    (         --2 指定导出列表
     ) where
 
-square :: Int -> Int   --3 给出类型签名
-square x = x * x       --4 提供函数定义
+type Alphabet = [Char] -- 定义字母表类型
